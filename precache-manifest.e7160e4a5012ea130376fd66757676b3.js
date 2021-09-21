1c14d1475ae0ce80a3e20d5709361a2786a30252
Pierre <Pierre>
2021-09-21 16:17:26 +0200

    Updates

---
+++ precache-manifest.e7160e4a5012ea130376fd66757676b3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb451095f712f8c969143940295c0919",
+    "url": "/react_piggy_bank/index.html"
+  },
+  {
+    "revision": "78ff26926ed460b3bca9",
+    "url": "/react_piggy_bank/static/js/2.14f1dc44.chunk.js"
+  },
+  {
+    "revision": "0610d5b0f1ed8702f180",
+    "url": "/react_piggy_bank/static/js/main.6ed52334.chunk.js"
+  },
+  {
+    "revision": "eae5e11a022209efbc73",
+    "url": "/react_piggy_bank/static/js/runtime~main.fa537acb.js"
+  }
+]);
